e20e177950273a12a1fe8891413a98e36bcb42ba
Steve Torrens <stevetorrens@Steves-MacBook-Air.local>
2018-12-03 12:32:22 +1300

    Working on user emissions service class

--- app/services/power/user_emissions.rb
+++ app/services/power/user_emissions.rb
@@ -4,17 +4,24 @@ class Power::UserEmissions
   end
 
   def call
-    obtain_profile
-    obtain_system_emissions
-    calculate_user_emissions
-  end
-
-  def obtain_profile
+    calculate_user_emissions(system_emissions)
   end 
   
-  def obtain_system_emissions
+  def system_emissions
+    HalfHourlyEmission.select(:date, :trading_period, :trader, :emissions_factor)
   end
   
-  def calculate_user_emissions
+  def calculate_user_emissions(systems_emissions)
+    systems_emissions.reduce([]) do |result, record|
+      user_emission =  @user_energy * 
+                        Profile.find_by(trading_period: record[:trading_period])[:profile] *
+                        record[:emissions_factor]
+      result << {
+        date: record[:date],
+        trading_period: record[:trading_period],
+        trader: record[:trader],
+        user_emission: user_emission
+      }
+    end  
   end  
 end
--- spec/services/power/user_emissions_spec.rb
+++ spec/services/power/user_emissions_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Power::UserEmissions do
+  EMISSIONS_FACTORS = [
+    0.003, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 
+    0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005,
+    0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005].freeze  
+
   before(:all) do
     @user_energy = 100.0
     @user_emissions = Power::UserEmissions.new(@user_energy)
@@ -10,13 +15,54 @@ RSpec.describe Power::UserEmissions do
     end 
   end
 
-  context "one trader, same profile and emissions per trading period" do
+  context 'one trader, same profile and emissions factor per trading period' do
     it 'calculates user emissions' do 
-      expected = @user_energy * Profile.last[:profile] * HalfHourlyEmission.last[:emissions_factor]
+      expected = @user_energy * 0.1 * 0.001
       actuals = @user_emissions.call
       actuals.each do |actual| 
-        expect(actual).to eq(expected)
+        expect(actual[:user_emission]).to eq(expected)
       end
     end
+  end 
+  
+  context 'two traders, emissions factor varies with trading period for second trader' do 
+    it 'calculates user emissions for both traders' do  
+      (1..48).each do |trading_period|
+        FactoryBot.create(:half_hourly_emission, 
+                          trading_period: trading_period, 
+                          trader: 'GENE',
+                          emissions_factor: EMISSIONS_FACTORS[trading_period - 1])
+      end
+      actual_CTCT = user_emissions_first_trading_period(@user_emissions.call, 'CTCT')
+      expect(actual_CTCT).to eq(@user_energy * 0.1 * 0.001) 
+      actual_GENE = user_emissions_first_trading_period(@user_emissions.call, 'GENE')
+      expect(actual_GENE).to eq(@user_energy * 0.1 * 0.003) 
+      actual_CTCT = user_emissions_total(@user_emissions.call, 'CTCT')
+      expect(actual_CTCT).to eq(@user_energy * 0.1 * 0.001 * 48)                        
+      # actual_CTCT_totals =  actuals
+      #                       .select{ |r| r.trader == 'CTCT' }
+      #                       .sum { |r| r.user_emissions}
+      # expect(actual_CTCT_totals).to eq(@user_emissions * 0.1 * 0.001 * 48)                       
+      # actual_GENE_tp1 = actuals
+      #                   .select{ |r| r.trader == 'GENE' && r.trading_period == 1 }
+      #                   .first[:user_emissions] 
+      # expect(actual_GENE_tp1).to eq(@user_emissions * 0.2 * 0.003)                                   
+      # actual_GENE_totals =  actuals
+      #                   .select{ |r| r.trader == 'GENE' }
+      #                   .sum { |r| r.user_emissions}
+      # expect(actual_CTCT_totals).to eq([PROFILES, EMISSIONS_FACTORS].transpose.map {|a| a.inject(:*)} * @user_energy) 
+    end
+
+    def user_emissions_first_trading_period(actuals, trader)
+      actuals
+        .select{ |r| r[:trader] == trader && r[:trading_period] == 1 }
+        .first[:user_emission]
+    end 
+    
+    def user_emissions_total(actuals, trader)
+      actuals
+        .select{ |r| r[:trader] == trader }
+        .sum { |r| r[:user_emission]}        
+    end  
   end  
 end
