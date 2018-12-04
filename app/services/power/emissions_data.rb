fca43dbdc7eab345a78effd997b6890a3c2d0d95
Steve Torrens <stevetorrens@Steves-Air.hub>
2018-12-04 14:14:20 +1300

    Working on emissions_data service object

--- app/services/power/emissions_data.rb
+++ app/services/power/emissions_data.rb
@@ -4,23 +4,41 @@ class Power::EmissionsData
   def call
     HalfHourlyEmission.destroy_all
     (1..ClearedOffer.maximum(:trading_period)).each do |trading_period|
-      get_hh_emissions_by_trader(trading_period).each do |record|
-        hh_emissions = get_hh_emissions(record)
-        hh_emissions[:emissions_factor] = (hh_emissions[:emissions] / get_energy(trading_period))
-        hh_emissions[:emissions_factor] = hh_emissions[:emissions_factor].round(6)
-        HalfHourlyEmission.create(hh_emissions)
+      get_hh_emissions_by_top_four_traders(trading_period).each do |record|
+        save_record(record, trading_period)  
       end
+      record = get_hh_emissions_other_traders(trading_period).first
+      record[:trader] = 'OTHR'
+      save_record(record, trading_period)  
     end
   end
 
   # Returns aggregated cleared_offers by trader for a given trading period.
-  def get_hh_emissions_by_trader(trading_period)
+  def get_hh_emissions_by_top_four_traders(trading_period)
     ClearedOffer
       .select('date, trading_period, trader, sum(emissions) as emissions, sum(cleared_energy) as cleared_energy')
       .group('date, trading_period, trader')
       .order(:trading_period)
       .where(trading_period: trading_period)
+      .where(trader: ['CTCT', 'GENE', 'TODD', 'MRPL'])
   end
+
+  def get_hh_emissions_other_traders(trading_period)
+    ClearedOffer
+      .select('date, trading_period, sum(emissions) as emissions, sum(cleared_energy) as cleared_energy')
+      .group('date, trading_period')
+      .order(:trading_period)
+      .where(trading_period: trading_period)
+      .where.not(trader: ['CTCT', 'GENE', 'TODD', 'MRPL'])
+  end
+
+  def save_record(record, trading_period)
+    hh_emissions = get_hh_emissions(record)
+    hh_emissions[:emissions_factor] = (hh_emissions[:emissions] / get_energy(trading_period))
+    hh_emissions[:emissions_factor] = hh_emissions[:emissions_factor].round(6)
+    new_hh_emisisons = HalfHourlyEmission.new(hh_emissions)
+    pp '*** Record not Valid ***', record, new_hh_emisisons.errors.messages unless new_hh_emisisons.save
+  end  
 
   def get_hh_emissions(record)
     { date: record[:date],
