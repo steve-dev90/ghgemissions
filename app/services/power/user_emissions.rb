c4f6bb717a5ff705b3c81d38218c2527d33e9302
Steve Torrens <stevetorrens70@gmail.com>
2019-01-14 07:58:51 +1300

    working on charts

--- app/services/power/user_emissions.rb
+++ app/services/power/user_emissions.rb
@@ -4,17 +4,18 @@ class Power::UserEmissions
   end
 
   def calculate_user_emissions_by_trader
-    system_emissions_by_trader.reduce([]) do |result, record|
-      user_emission = @user_energy *
-                      Profile.find_by(trading_period: record[:trading_period])[:profile] *
-                      record[:emissions_factor]
-      result << {
-        date: record[:date],
-        trading_period: record[:trading_period],
-        trader: record[:trader],
-        user_emission: user_emission
-      }
-    end
+    # system_emissions_by_trader.reduce([]) do |result, record|
+    #   user_emission = @user_energy *
+    #                   Profile.find_by(trading_period: record[:trading_period])[:profile] *
+    #                   record[:emissions_factor]
+    #   result << {
+    #     date: record[:date],
+    #     trading_period: record[:trading_period],
+    #     trader: record[:trader],
+    #     user_emission: user_emission
+    #   }
+    # end
+    system_emissions_by_trader
   end
 
   def calculate_user_emissions
@@ -33,10 +34,13 @@ class Power::UserEmissions
   private
 
   def system_emissions_by_trader
+    # HalfHourlyEmission
+    #   .select(:date, :trading_period, :trader, :emissions_factor)
+    #   .order(trading_period: :asc)
     HalfHourlyEmission
-      .select(:date, :trading_period, :trader, :emissions_factor)
-      .order(trader: :asc)
-      .order(trading_period: :asc)
+     .joins("INNER JOIN profiles ON profiles.trading_period = half_hourly_emissions.trading_period")
+     .select('half_hourly_emissions.trader, sum(profiles.profile * half_hourly_emissions.emissions_factor) as emissions_factor')
+     .group("trader")
   end
 
   def system_emissions
