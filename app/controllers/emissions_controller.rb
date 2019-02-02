d214e08f2761f667c17626726b4560f5691f340e
Steve Torrens <stevetorrens70@gmail.com>
2019-02-02 19:59:42 +1300

    Working on emissions front end

--- app/controllers/emissions_controller.rb
+++ app/controllers/emissions_controller.rb
@@ -3,7 +3,7 @@ class EmissionsController < ApplicationController
     emissions = Power::UserEmissions.new(emissions_params[:user_energy].to_f)
     @user_emissions = emissions.calculate_user_emissions
     @trader_emissions = emissions.calculate_user_emissions_factors_by_trader
-    @total_emissions = @user_emissions.sum{ |e| e[:user_emission]}
+    @total_emissions = @user_emissions.sum{ |e| e[:user_emission]}.round(1)
   end
 
   private
