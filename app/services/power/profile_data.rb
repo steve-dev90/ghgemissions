8c2b0af8f7c10ed10309431371b8fdcb2be68526
Steve Torrens <stevetorrens70@gmail.com>
2019-03-12 12:27:17 +1300

    working on profile

--- app/services/power/profile_data.rb
+++ app/services/power/profile_data.rb
@@ -5,57 +5,108 @@ class Power::ProfileData
   end
 
   def call
-    obtain_profile_records
-    divide_profile_by_total_profile
-    save_records
-    puts 'Profile table updated!'
+    csv = CSV.read(@file, converters: :numeric)
+    obtain_profile_records(csv)
+    pp @profile_records
   end
 
-  def obtain_profile_records
-    Roo::Spreadsheet.open(@file).sheet('Sheet1').each do |row|
-      next if row[0] == 'GXP'
-
-      record = extract_record(row)
-      index = record_index(record)
-      if index.nil?
-        create_new_record(record)
-      else
-        update_record(index, record)
-      end
+  def obtain_profile_records(csv)
+    profile_annual_sum = csv.sum { |row| row[5] }
+    months = csv.map { |row| Date.parse(row[3]).month }.uniq
+    months.each do |month|
+      profile_monthly_sum = profile_monthly_sum(csv, month)
+      pp 'Hello', profile_monthly_sum
+      monthly_profile = profile_monthly_sum / profile_annual_sum
+      @profile_records << {month: month, profile: monthly_profile, period: 'month'}
+      add_trading_period_profile_records(csv, profile_monthly_sum, month)
+      add_wkend_night_profile_records(csv, profile_monthly_sum, month)
     end
   end
 
-  def extract_record(row)
-    { trading_period: row[4].to_i, profile: row[5].to_f }
+  def profile_monthly_sum(csv, month)
+    csv
+      .select { |row| Date.parse(row[3]).month == month }
+      .sum { |row| row[5]}
   end
 
-  def record_index(record)
-    @profile_records.index do |profile_record|
-      profile_record[:trading_period] == record[:trading_period]
+  def add_trading_period_profile_records(csv, profile_monthly_sum, month)
+    trading_periods = csv.map { |row| row[4] }.uniq
+    trading_periods.each do |trading_period|
+      profile_trading_period_sum = trading_period_profile_sum(csv, month, trading_period)
+      trading_period_profile = profile_trading_period_sum / profile_monthly_sum
+      @profile_records << {month: month, profile: trading_period_profile, period: trading_period.to_s}
     end
   end
 
-  def create_new_record(record)
-    @profile_records << record
+  def trading_period_profile_sum(csv, month, trading_period)
+    csv
+     .select { |row| Date.parse(row[3]).month == month && row[4] == trading_period }
+     .sum { |row| row[5]}
   end
 
-  def update_record(index, record)
-    @profile_records[index][:profile] += record[:profile]
+  def add_wkend_night_profile_records(csv, profile_monthly_sum, month)
+    profile_wkend_night_sum = csv
+      .select { |row| Date.parse(row[3]).month == month && (row[4] < 15 || row[4] > 19) }
+      .sum { |row| row[5]}
+    wkend_night_profile = profile_wkend_night_sum / profile_monthly_sum
+    @profile_records << {month: month, profile: wkend_night_profile, period: 'wkend-night'}
   end
 
-  def obtain_profile_sum
-    @profile_records.sum { |record| record[:profile] }
-  end
 
-  def divide_profile_by_total_profile
-    pp sum = obtain_profile_sum
-    @profile_records.each { |record| record[:profile] = record[:profile] / sum }
-  end
+  # def obtain_profile_records
+  #   CSV.foreach(@file, converters: :numeric) do |row|
+  #     record = extract_record(row)
+  #     hash_record = { month: record[:month], profile: record[:profile] }
+  #     # create_or_update_profile_record(record[:month], 'month', hash_record)
+  #     # create_or_update_profile_record(record[:month], record[:trading_period], hash_record)
+  #     # create_or_update_profile_record(record[:month], record[:period], hash_record)
+  #   end
+  # end
 
-  def save_records
-    @profile_records.each do |record|
-      profile = Profile.find_or_create_by(trading_period: record[:trading_period])
-      pp '*** Record not Valid ***', record, profile.errors.messages unless profile.update_attributes(record)
-    end
-  end
+  # def extract_record(row)
+  #   date = Date.parse(row[3])
+  #   wkend = date.saturday? || date.sunday
+  #   # Day is deemed from 7 am (tp = 15) to 7 pm ( tp = 39)
+  #   day = row[4] >= 15 && row[4] <= 19
+  #   period = 'wkend-day' if wkend && day
+  #   period = 'wkend-night' if wkend && !day
+  #   period = 'wkday-day' if !wkend && day
+  #   period = 'wkday-night' if !wkend && !day
+  #   { trading_period: row[4],
+  #     profile: row[5],
+  #     month: date.month,
+  #     period: period
+  #   }
+  # end
+
+  # def create_or_update_profile_record(month, period, hash_record)
+  #   index = @profile_records.index do |profile_record|
+  #     profile_record[:month] == month && profile_record[:period] == period
+  #   end
+  #   if index.nil?
+  #     @profile_records << hash_record.merge(period: period)
+  #   else
+  #     @profile_records[index][:profile] += hash_record[:profile]
+  #   end
+  # end
+
+  # def update_record(index, record)
+  #   @profile_records[index][:profile] += record[:profile]
+  # end
+
+  # def obtain_profile_sum
+  #   @profile_records.sum { |record| record[:profile] }
+  # end
+
+  # def divide_profile_by_total_profile
+  #   pp sum = obtain_profile_sum
+  #   @profile_records.each { |record| record[:profile] = record[:profile] / sum }
+  # end
+
+  # def save_records
+  #   @profile_records.each do |record|
+  #     profile = Profile.find_or_create_by(trading_period: record[:trading_period])
+  #     pp '*** Record not Valid ***', record, profile.errors.messages unless profile.update_attributes(record)
+  #   end
+  # end
 end
--- lib/tasks/power/import_ea_profile.rake
+++ lib/tasks/power/import_ea_profile.rake
@@ -1,7 +1,7 @@
 namespace :power do
   desc 'Import EA rps profile for GXP CPK0111 stored in spreadsheet'
   task import_ea_profile: :environment do
-    profile = Power::ProfileData.new('./lib/assets/CPK0111 RPS.xlsx')
+    profile = Power::ProfileData.new('./lib/assets/CPK0111_RPS.csv')
     profile.call
   end
 end
