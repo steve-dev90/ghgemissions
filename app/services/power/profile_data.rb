052d226941fe2c56bc0144616a17e32a5c83d3b3
Steve Torrens <stevetorrens70@gmail.com>
2019-03-12 19:47:17 +1300

    working on profile

--- app/services/power/profile_data.rb
+++ app/services/power/profile_data.rb
@@ -1,10 +1,10 @@
 class Power::ProfileData
+  PERIOD_TYPES = %w[ wkday_night wkday_day wkend_night wkend_day].freeze
+
   def initialize(file)
     @file = file
     @profile_records = []
   end
-
-  # Day is deemed from 7 am (tp = 15) to 7 pm ( tp = 39)
 
   def call
     csv = CSV.read(@file, converters: :numeric)
@@ -18,42 +18,23 @@ class Power::ProfileData
     months = csv.map { |row| Date.parse(row[3]).month }.uniq
     months.each do |month|
       profile_monthly_sum = profile_monthly_sum(csv, month)
-      monthly_profile = profile_monthly_sum / profile_annual_sum
-      @profile_records << {month: month, profile: monthly_profile, period: 'month'}
+      add_profile_records(csv, profile_annual_sum, month, 'month')
       add_trading_period_profile_records(csv, profile_monthly_sum, month)
-      add_wkend_night_profile_records(csv, profile_monthly_sum, month)
+      PERIOD_TYPES.each { |period| add_profile_records(csv, profile_monthly_sum, month, period) }
     end
   end
 
   def profile_monthly_sum(csv, month)
     csv
-      .select { |row| Date.parse(row[3]).month == month }
+      .select { |row| profile_test(row, month, 'month') }
       .sum { |row| row[5]}
   end
 
   def add_trading_period_profile_records(csv, profile_monthly_sum, month)
     trading_periods = csv.map { |row| row[4] }.uniq
     trading_periods.each do |trading_period|
-      profile_trading_period_sum = trading_period_profile_sum(csv, month, trading_period)
-      trading_period_profile = profile_trading_period_sum / profile_monthly_sum
-      @profile_records << {month: month, profile: trading_period_profile, period: trading_period.to_s}
+      add_profile_records(csv, profile_monthly_sum, month, trading_period.to_s)
     end
-  end
-
-  def trading_period_profile_sum(csv, month, trading_period)
-    csv
-     .select { |row| Date.parse(row[3]).month == month && row[4] == trading_period }
-     .sum { |row| row[5]}
-  end
-
-  def add_wkend_night_profile_records(csv, profile_monthly_sum, month)
-    # profile_wkend_night_sum = csv
-    #   .select { |row| Date.parse(row[3]).month == month && (row[4] < 15 || row[4] > 19) }
-    #   .sum { |row| row[5]}
-    # wkend_night_profile = profile_wkend_night_sum / profile_monthly_sum
-    # @profile_records << {month: month, profile: wkend_night_profile, period: 'wkend_night'}
-    add_profile_records(csv, profile_monthly_sum, month, 'wkend_night')
-
   end
 
   def add_profile_records(csv, profile_sum, month, period)
@@ -63,8 +44,17 @@ class Power::ProfileData
     @profile_records << {month: month, profile: profile, period: period}
   end
 
+  # Day is deemed from 7 am (tp = 15) to 7 pm ( tp = 39)
   def profile_test(row, month, period)
-    Date.parse(row[3]).month == month && (row[4] < 15 || row[4] > 19) if period == 'wkend_night'
+    month_test = Date.parse(row[3]).month == month
+    night_test = row[4] < 15 || row[4] > 39
+    wkend_test = Date.parse(row[3]).saturday? || Date.parse(row[3]).sunday?
+    return month_test && night_test && wkend_test if period == 'wkend_night'
+    return month_test && !night_test && wkend_test if period == 'wkend_day'
+    return month_test && night_test && !wkend_test if period == 'wkday_night'
+    return month_test && !night_test && !wkend_test if period == 'wkday_day'
+    return month_test if period == 'month' if period == 'month'
+    Date.parse(row[3]).month == month && row[4] == period.to_i
   end
 
   def save_records
--- spec/services/power/profile_data_spec.rb
+++ spec/services/power/profile_data_spec.rb
@@ -9,15 +9,24 @@ RSpec.describe Power::ProfileData do
 
   it 'calculates normalised monthly, trading period, weekday and weekend profiles correctly' do
     #sum profile over all trading periods, trading dates for each month
-    sum_monthly_profile = [0, 0, 0, 0, 0, 0, 0, 0, 20.0, 0, 0, 0]
+    sum_monthly_profile = [0, 0, 0, 0, 0, 0, 0, 0, 90.0, 0, 0, 0]
     #sum profile over trading period 1, for all trading dates, for each month
-    sum_tp1_profile = [0, 0, 0, 0, 0, 0, 0, 0, 4.0, 0, 0, 0]
+    sum_tp1_profile = [0, 0, 0, 0, 0, 0, 0, 0, 8.0, 0, 0, 0]
     #sum profile over weekend night trading periods, for each month
     sum_wkend_night_profile = [0, 0, 0, 0, 0, 0, 0, 0, 20.0, 0, 0, 0]
+    #sum profile over weekend day trading periods, for each month
+    sum_wkend_day_profile = [0, 0, 0, 0, 0, 0, 0, 0, 25.0, 0, 0, 0]
+    #sum profile over all trading periods, trading dates and months
+    sum_wkday_night_profile = [0, 0, 0, 0, 0, 0, 0, 0, 20.0, 0, 0, 0]
+    #sum profile over weekend day trading periods, for each month
+    sum_wkday_day_profile = [0, 0, 0, 0, 0, 0, 0, 0, 25.0, 0, 0, 0]
     #sum profile over all trading periods, trading dates and months
     pp sum_profile = sum_monthly_profile.sum { |n| n }
-    expect(Profile.find_by(period: 'month', month: '9')[:profile]).to eq(sum_monthly_profile[8] / sum_profile)
-    expect(Profile.find_by(period: 'wkend_night', month: '9')[:profile]).to eq(sum_wkend_night_profile[8] / sum_monthly_profile[8])
-    expect(Profile.find_by(period: '1', month: '9')[:profile]).to eq(sum_tp1_profile[8] / sum_monthly_profile[8])
+    expect(Profile.find_by(period: 'month', month: '9')[:profile].round(4)).to eq((sum_monthly_profile[8] / sum_profile).round(4))
+    expect(Profile.find_by(period: 'wkend_night', month: '9')[:profile].round(4)).to eq((sum_wkend_night_profile[8] / sum_monthly_profile[8]).round(4))
+    expect(Profile.find_by(period: 'wkend_day', month: '9')[:profile].round(4)).to eq((sum_wkend_day_profile[8] / sum_monthly_profile[8]).round(4))
+    expect(Profile.find_by(period: 'wkday_night', month: '9')[:profile].round(4)).to eq((sum_wkday_night_profile[8] / sum_monthly_profile[8]).round(4))
+    expect(Profile.find_by(period: 'wkday_day', month: '9')[:profile].round(4)).to eq((sum_wkday_day_profile[8] / sum_monthly_profile[8]).round(4))
+    expect(Profile.find_by(period: '1', month: '9')[:profile].round(4)).to eq((sum_tp1_profile[8] / sum_monthly_profile[8]).round(4))
   end
 end
