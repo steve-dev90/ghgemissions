ba7802a1fa2b34a0ef6602e2de7391716812e4d5
Steve Torrens <stevetorrens70@gmail.com>
2019-07-01 21:05:34 +1200

    working on emi imports

--- app/services/power/cleared_offer_data_emi.rb
+++ app/services/power/cleared_offer_data_emi.rb
@@ -1,17 +1,30 @@
 class Power::ClearedOfferDataEMI
+  # Electricity Authority emi endpoint for cleared offers folder
+  # Note : excludes the year, added progamatically below
+  EMI_CLEARED_OFFER_FOLDER = 'https://emidatasets.blob.core.windows.net/publicdata?restype=container&comp=list&prefix=Datasets/Wholesale/Final_pricing/Cleared_Offers/'
+  #Electricity Authority emi file url path, excluding the file name
+  EMI_CLEARED_OFFER_FILE = 'https://emidatasets.blob.core.windows.net/publicdata/Datasets/Wholesale/Final_pricing/Cleared_Offers/'
 
   def call
-    p 'hello there'
-    url5='https://emidatasets.blob.core.windows.net/publicdata?restype=container&comp=list&prefix=Datasets/Wholesale/Final_pricing/Cleared_Offers/2019'
-
-    response = HTTParty.get(url5)
-    # pp response.body
-    # response.code, response.message, response.headers.inspect
-    doc = Nokogiri::XML(response.body)
-    blobs = doc.xpath('//Url')
-    blobs.each do |blob|
-      pp blob
+    # Needs to be current date year + year(s) in database
+    year = Date.parse(Time.new.to_s).year.to_s
+    url = EMI_CLEARED_OFFER_FOLDER + year
+    response = HTTParty.get(url)
+    # NEED TO SELECT JUNE
+    emi_files = Nokogiri::XML(response.body).xpath('//Url').reduce([]) do |files, blob|
+      files << blob.content.gsub(EMI_CLEARED_OFFER_FILE,'')
     end
+    pp emi_files
+
+    # Get URL
+    # Make sure you have the right year and month
+    # get array of emi cleared offer files
+    # get array of temp cleared offer files
+    # Work out emi cleared offer files to load
+    # Load and process emi file
+    # Emi file gets saved in temp database
+    # Send email
+
   end
 
 end
