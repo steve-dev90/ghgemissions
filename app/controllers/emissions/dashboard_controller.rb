82d5bea6d61e6c3bf30937512308cc973e9ee66e
Steve Torrens <stevetorrens70@gmail.com>
2019-08-18 16:30:49 +1200

    added dashboard

--- app/controllers/emissions/dashboard_controller.rb
+++ app/controllers/emissions/dashboard_controller.rb
@@ -2,21 +2,22 @@ module Emissions
   class DashboardController < ApplicationController
     def index
       previous_month = Date.parse(Time.new.to_s).prev_month.month
-      energy = Power::PreviousMonthEnergyEstimate.new(
-        emissions_params[:user_energy].to_f,
-        emissions_params[:start_date_submit],
-        emissions_params[:end_date_submit],
+      previous_month_power = Power::PreviousMonthEnergyEstimate.new(
+        dashboard_params[:user_energy].to_f,
+        dashboard_params[:start_date_submit],
+        dashboard_params[:end_date_submit],
         previous_month
       )
-      emissions = Power::UserEmissions.new(energy.call, previous_month)
+
+      @previous_month_power = previous_month_power.call
+      emissions = Power::UserEmissions.new(@previous_month_power, previous_month)
       @user_emissions = emissions.calculate_user_emissions
-      @trader_emissions = emissions.calculate_user_emissions_factors_by_trader
       @total_emissions = @user_emissions.sum{ |e| e[:user_emission]}.round(1)
     end
 
     private
 
-    def emissions_params
+    def dashboard_params
       params.permit(:user_energy, :start_date_submit, :end_date_submit)
     end
   end
--- app/helpers/power_emissions_helper.rb
+++ app/helpers/power_emissions_helper.rb
@@ -1,2 +1,31 @@
 module PowerEmissionsHelper
+  def chart_data(query_data, label, data_name, background_color)
+    labels = []
+    data = []
+    query_data.each do |data_item|
+      labels << data_item[label].to_s
+      data << data_item[data_name]
+    end
+
+    { labels: labels,
+      datasets: [
+        { label: "My First dataset",
+          backgroundColor: background_color,
+          borderColor: "rgba(220,220,220,1)",
+          data: data
+        }] }
+  end
+
+  def user_emissions_options
+    {
+      scales:
+        { yAxes: [{ scaleLabel: { display: true,
+                    labelString: 'Emisssions kgCO2' }
+                  }],
+          xAxes: [{ scaleLabel: { display: true,
+                    labelString: 'Time hours: minutes' }
+                  }]
+        }
+    }
+  end
 end
