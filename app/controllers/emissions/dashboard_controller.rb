d573403c4583897544eb2fcc64fd1c4caef1acc2
Steve Torrens <stevetorrens70@gmail.com>
2020-05-22 21:05:36 +1200

    Added first cut bar chart for emissions by source

--- app/controllers/emissions/dashboard_controller.rb
+++ app/controllers/emissions/dashboard_controller.rb
@@ -15,18 +15,33 @@ module Emissions
       )
       @previous_month_power = previous_month_power.call
 
-      previous_month_gas = Energy::PreviousMonthEnergyEstimate.new(
-        dashboard_params[:gas_user_energy].to_f,
-        dashboard_params[:gas_start_date],
-        dashboard_params[:gas_end_date],
-        previous_month,
-        'gas'
-      )
-      @previous_month_gas = previous_month_gas.call
+      if !dashboard_params[:gas_user_energy].empty?
+        previous_month_gas = Energy::PreviousMonthEnergyEstimate.new(
+          dashboard_params[:gas_user_energy].to_f,
+          dashboard_params[:gas_start_date],
+          dashboard_params[:gas_end_date],
+          previous_month,
+          'gas'
+        )
+        previous_month_gas = previous_month_gas.call
+        pp previous_month_gas
+        gas_emissions = { emissions_source: 'Gas', user_emission: 53.96 * 0.0036 * previous_month_gas}
+      else
+        gas_emissions = { emissions_source: 'Gas', user_emission: 0}
+      end
 
       emissions = Power::UserEmissions.new(@previous_month_power, previous_month)
-      @user_emissions = emissions.calculate_user_emissions
-      @total_emissions = @user_emissions.sum{ |e| e[:user_emission]}.round(1)
+      # @user_emissions = emissions.calculate_user_emissions
+      @total_emissions = emissions
+                            .calculate_user_emissions
+                            .sum{ |e| e[:user_emission]}
+                            .round(1)
+
+      @user_emissions = [
+        { emissions_source: 'Power', user_emission: @total_emissions },
+        gas_emissions
+      ]
+
     end
 
     private
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,28 @@
 module DashboardHelper
+  def bar_data(query_data, label_title, data_title)
+
+    { labels: query_data.reduce([]) {|labels, data_pt| labels << data_pt[label_title].to_s},
+      datasets: [
+        { label: "User Emissions",
+          barPercentage: 0.5,
+          barThickness: 6,
+          maxBarThickness: 8,
+          minBarLength: 2,
+          data: query_data.reduce([]) {|data, data_pt| data << data_pt[data_title].to_s}
+        }] }
+  end
+
+  def bar_options
+    {
+      height: 300,
+      legend: {display: false},
+      scales:{
+        xAxes: [{
+            gridLines: {
+                offsetGridLines: true
+            }
+        }]
+      }
+    }
+  end
 end
